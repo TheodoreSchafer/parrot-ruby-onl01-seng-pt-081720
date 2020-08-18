2e37672d0c56343936a8911de4989ff126ed4d5d
garrulous-rockstar-5801 <theodore.schafer@gmail.com>
2020-08-18 15:46:16 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(phrase = "Squawk!")
-  return "Pretty bird!"
   puts "#{phrase}"
+  return "Pretty bird!"
 end
 
 parrot("phrase")
